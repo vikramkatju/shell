dc512a46229093159776976ece21ca450ec3000e
vikramkatju <vikthirtysix@gmail.com>
2024-11-22 19:15:35 -0500

    Added a line to the bash file for fixing my answer to Question 7

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -61,7 +61,7 @@ cp ./data/raw/event_log_*.log ./data/processed/event_logs
 #
 #rf -rf ./data
 
-rm ./data/raw/*ipaddr*.log
+rm ./data/raw/*ipaddr*.*
 rm ./data/processed/user_logs/*ipaddr*.log 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
